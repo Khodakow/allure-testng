0ab8a1388a1652f447053e51a900c1a6132d6722
s.lugovskiy <Fa64dea2>
2015-03-04 17:13:36 +0300

    locator fix

--- src/main/java/pages/dashboard/elements/Graph.java
+++ src/main/java/pages/dashboard/elements/Graph.java
@@ -10,7 +10,7 @@ import ru.yandex.qatools.htmlelements.element.HtmlElement;
  * Created by s.lugovskiy on 04.03.2015 11:15.
  */
 @Name("Top Dashboard Graph")
-@Block(@FindBy(id = "section_graph"))
+@Block(@FindBy(id = "form_top_graph_1"))
 public class Graph extends HtmlElement {
 
     @Step
