adb61b5c39f85ecb475b51f80df786f5584f922e
a.hodakov <alexkhodakow@gmail.com>
2015-03-10 11:19:55 +0300

    Carcas Test

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -10,13 +10,25 @@ import ru.yandex.qatools.allure.annotations.Step;
 public class BasePage{
 
     private WebDriver driver;
+    private String baseUrl;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.baseUrl = System.getProperty("baseUrl");
     }
 
     @Step
     public Object  evaluateJavascript(String script){
         return ((JavascriptExecutor)driver).executeScript(script);
     }
+
+
+    @Step
+    public void goToSection(String url) {
+        String testUrl = baseUrl + url;
+        String newtestUrl = testUrl.replaceAll("//","/");
+        driver.get(newtestUrl);
+    }
+
+
 }
---
+++ src/test/java/wm/CarcasTest.java
@@ -0,0 +1,79 @@
+package wm;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import pages.dashboard.DashboardPage;
+import pages.front.FrontPage;
+import pages.offercard.OfferCardPage;
+import roles.Webmaster;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.model.SeverityLevel;
+import setup.Utils;
+import wm.listener.AllureOnFailListener;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+@Test
+@Listeners(AllureOnFailListener.class)
+public class CarcasTest extends BaseTest {
+
+
+
+    private FrontPage front;
+    private DashboardPage dashboard;
+    private OfferCardPage card;
+    private Webmaster wm;
+    private Utils utils;
+    private WebDriver driver;
+
+
+
+
+
+
+    @DataProvider
+    public Object[][] getUrl() {
+        return new Object[][]{
+                {"/ru/webmaster/instrumentyi/push_ads/getlink_445571172.0.htm"},
+                {"/ru/webmaster/tools/feeds/coupons_feed_3641948.0.htm"},
+                {"/ru/webmaster/tools/rotators/banners_rotator_416459372.0.htm"},
+        };
+    }
+
+
+
+    @BeforeClass
+    public void setUp(){
+        super.init();
+        driver = super.getDriver();
+        utils = super.getUtils();
+        front = new FrontPage(driver);
+        dashboard = new DashboardPage(driver);
+        card = new OfferCardPage(driver);
+        wm = new Webmaster();
+        utils.openMainPage();
+        front.login(wm);
+
+    }
+
+
+
+    @Test(dataProvider = "getUrl")
+    @Severity(value = SeverityLevel.CRITICAL)
+    public void carcaseTest(String url) throws InterruptedException {
+
+        dashboard.goToSection(url);
+        dashboard.waitSpinner();
+        assertTrue(url.length()>0);
+
+
+    }
+
+
+
+
+
+
+
+
+}
